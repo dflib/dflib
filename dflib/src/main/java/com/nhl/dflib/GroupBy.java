21955b6903ee0fb6a2e05bda4e01458cfcc9ba44
Andrus Adamchik <andrus@objectstyle.com>
2019-01-19 18:18:32 -0500

    'sort' and 'sortByColumns' to reorder DataFrames
    * adding sorting to GroupBy ... while redundant, it may come in handy for analytics within the groups

--- dflib/src/main/java/com/nhl/dflib/GroupBy.java
+++ dflib/src/main/java/com/nhl/dflib/GroupBy.java
@@ -2,9 +2,11 @@ package com.nhl.dflib;
 
 import com.nhl.dflib.aggregate.Aggregator;
 import com.nhl.dflib.aggregate.ColumnAggregator;
+import com.nhl.dflib.map.RowToValueMapper;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +30,44 @@ public class GroupBy {
 
     public DataFrame getGroup(Object key) {
         return groups.get(key);
+    }
+
+    public <V extends Comparable<? super V>> GroupBy sort(RowToValueMapper<V> sortKeyExtractor) {
+        Map<Object, DataFrame> sorted = new LinkedHashMap<>((int) (groups.size() / 0.75));
+
+        for (Map.Entry<Object, DataFrame> e : groups.entrySet()) {
+            sorted.put(e.getKey(), e.getValue().sort(sortKeyExtractor));
+        }
+
+        return new GroupBy(ungroupedColumns, sorted);
+    }
+
+    public <V extends Comparable<? super V>> GroupBy sortByColumns(String... columns) {
+        if (columns.length == 0) {
+            return this;
+        }
+
+        Map<Object, DataFrame> sorted = new LinkedHashMap<>((int) (groups.size() / 0.75));
+
+        for (Map.Entry<Object, DataFrame> e : groups.entrySet()) {
+            sorted.put(e.getKey(), e.getValue().sortByColumns(columns));
+        }
+
+        return new GroupBy(ungroupedColumns, sorted);
+    }
+
+    public <V extends Comparable<? super V>> GroupBy sortByColumns(int... columns) {
+        if (columns.length == 0) {
+            return this;
+        }
+
+        Map<Object, DataFrame> sorted = new LinkedHashMap<>((int) (groups.size() / 0.75));
+
+        for (Map.Entry<Object, DataFrame> e : groups.entrySet()) {
+            sorted.put(e.getKey(), e.getValue().sortByColumns(columns));
+        }
+
+        return new GroupBy(ungroupedColumns, sorted);
     }
 
     public DataFrame agg(ColumnAggregator... aggregators) {
