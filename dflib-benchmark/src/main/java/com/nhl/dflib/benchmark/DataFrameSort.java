48ff62728f4bac15003bfcb5b067f36a4256cef2
Andrus Adamchik <andrus@objectstyle.com>
2019-02-10 16:30:23 +0300

    adding "sort" benchmark

---
+++ dflib-benchmark/src/main/java/com/nhl/dflib/benchmark/DataFrameSort.java
@@ -0,0 +1,51 @@
+package com.nhl.dflib.benchmark;
+
+import com.nhl.dflib.DataFrame;
+import com.nhl.dflib.Index;
+import com.nhl.dflib.map.RowToValueMapper;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.StreamSupport;
+
+// in my tests time for iteration #3 time spikes (why?), so make sure it happens during warmup
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 3, time = 1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(2)
+@State(Scope.Thread)
+public class DataFrameSort {
+
+    @Param("1000000")
+    public int rows;
+
+    private DataFrame df;
+
+    @Setup
+    public void setUp() {
+        Index index = Index.withNames("id", "data", "rev_id", "text");
+        df = DataFrame.fromStream(
+                index,
+                StreamSupport.stream(new DataSetSpliterator(rows), false)
+        );
+    }
+
+    @Benchmark
+    public Object sort() {
+        return df
+                .sort(RowToValueMapper.columnReader(2))
+                .materialize()
+                .iterator();
+    }
+}
