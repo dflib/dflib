c8cf97d60dafea237f2aa33610166d0d20628f74
Andrus Adamchik <andrus@objectstyle.com>
2019-04-03 08:20:17 +0300

    utility methods for RowPredicate composition

--- dflib/src/main/java/com/nhl/dflib/filter/RowPredicate.java
+++ dflib/src/main/java/com/nhl/dflib/filter/RowPredicate.java
@@ -5,5 +5,29 @@ import com.nhl.dflib.row.RowProxy;
 @FunctionalInterface
 public interface RowPredicate {
 
+    static <V> RowPredicate forColumn(int pos, ValuePredicate<V> columnPredicate) {
+        return r -> columnPredicate.test((V) r.get(pos));
+    }
+
     boolean test(RowProxy r);
+
+    default <V> RowPredicate and(int pos, ValuePredicate<V> another) {
+        return r -> this.test(r) && another.test((V) r.get(pos));
+    }
+
+    default <V> RowPredicate and(String label, ValuePredicate<V> another) {
+        return r -> this.test(r) && another.test((V) r.get(label));
+    }
+
+    default <V> RowPredicate or(int pos, ValuePredicate<V> another) {
+        return r -> this.test(r) || another.test((V) r.get(pos));
+    }
+
+    default <V> RowPredicate or(String label, ValuePredicate<V> another) {
+        return r -> this.test(r) || another.test((V) r.get(label));
+    }
+
+    default <V> RowPredicate negate() {
+        return r -> !this.test(r);
+    }
 }
