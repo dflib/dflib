9a54290244b6c93d6bedf38ae2946767619b86dd
Andrus Adamchik <andrus@objectstyle.com>
2019-04-10 13:47:06 +0300

    bug in height init

--- dflib/src/main/java/com/nhl/dflib/row/DataFrameRowProxy.java
+++ dflib/src/main/java/com/nhl/dflib/row/DataFrameRowProxy.java
@@ -11,7 +11,7 @@ public class DataFrameRowProxy implements RowProxy {
 
     public DataFrameRowProxy(DataFrame dataFrame) {
         this.dataFrame = dataFrame;
-        this.height = height;
+        this.height = dataFrame.height();
         this.rowIndex = -1;
     }
 
