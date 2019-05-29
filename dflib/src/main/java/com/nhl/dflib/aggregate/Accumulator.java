ac9c3b922cd4f905a7ec0eef84223e3802a016c3
Andrus Adamchik <andrus@objectstyle.com>
2019-05-29 10:24:47 +0300

    removing unused interfaces

--- dflib/src/main/java/com/nhl/dflib/aggregate/Accumulator.java
+++
@@ -1,7 +0,0 @@
-package com.nhl.dflib.aggregate;
-
-@FunctionalInterface
-public interface Accumulator<A, V> {
-
-    void accumulate(A to, V v);
-}
--- dflib/src/main/java/com/nhl/dflib/aggregate/AggregationRunner.java
+++
@@ -1,9 +0,0 @@
-package com.nhl.dflib.aggregate;
-
-import com.nhl.dflib.DataFrame;
-
-@FunctionalInterface
-public interface AggregationRunner {
-
-    Object[] aggregate(DataFrame df);
-}
