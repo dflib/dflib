541a10684d54726e1423e156f1ee44d6cc16bd21
Andrus Adamchik <andrus@objectstyle.com>
2019-06-06 12:18:03 +0300

    javadoc

--- dflib-jdbc/src/main/java/com/nhl/dflib/jdbc/SaveOp.java
+++ dflib-jdbc/src/main/java/com/nhl/dflib/jdbc/SaveOp.java
@@ -3,7 +3,9 @@ package com.nhl.dflib.jdbc;
 import com.nhl.dflib.DataFrame;
 
 /**
- * Defines possible outcomes for DataFrame rows during save via {@link com.nhl.dflib.jdbc.connector.TableSaver}.
+ * Defines possible outcomes for DataFrame rows during when saving via {@link com.nhl.dflib.jdbc.connector.TableSaver}.
+ * A <code>Series&lt;SaveOp></code> can be retrieved from the supplier returned by
+ * {@link com.nhl.dflib.jdbc.connector.TableSaver#save(DataFrame)}
  *
  * @see com.nhl.dflib.jdbc.connector.TableSaver#save(DataFrame)
  * @since 0.6
