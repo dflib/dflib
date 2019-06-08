2993f47cc7167905b2764820928ee1c75c89154c
Andrus Adamchik <andrus@objectstyle.com>
2019-06-08 12:05:24 -0400

    Jupyter docs section

---
+++ dflib-doc/src/main/java/com/nhl/dflib/docs/Examples.java
@@ -0,0 +1,24 @@
+package com.nhl.dflib.docs;
+
+import com.nhl.dflib.DataFrame;
+import com.nhl.dflib.Printers;
+
+import java.util.stream.IntStream;
+
+public class Examples {
+
+    public static void main(String[] args) {
+        gettingStarted();
+    }
+
+    private static void gettingStarted() {
+        DataFrame df1 = DataFrame
+                .newFrame("a", "b", "c")
+                .foldIntStreamByRow(0, IntStream.range(1, 10000));
+
+        DataFrame df2 = df1
+                .filter("a", (Integer v) -> v % 2 == 0);
+
+        System.out.println(Printers.tabular.toString(df2));
+    }
+}
