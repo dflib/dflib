d51b0c122ce75c43b9c7cfea917b192f2923abfb
Andrus Adamchik <andrus@objectstyle.com>
2019-06-08 13:39:09 -0400

    chapter on Series

--- dflib-doc/src/main/java/com/nhl/dflib/docs/GettingStartedExamples.java
+++ dflib-doc/src/main/java/com/nhl/dflib/docs/GettingStartedExamples.java
@@ -5,7 +5,7 @@ import com.nhl.dflib.Printers;
 
 import java.util.stream.IntStream;
 
-public class Examples {
+public class GettingStartedExamples {
 
     public static void main(String[] args) {
         gettingStarted();
---
+++ dflib-doc/src/main/java/com/nhl/dflib/docs/SeriesExamples.java
@@ -0,0 +1,4 @@
+package com.nhl.dflib.docs;
+
+public class SeriesExamples {
+}
