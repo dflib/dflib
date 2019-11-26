8d37c1268665e122fbf5aef53646a24369125b15
Andrus Adamchik <andrus@objectstyle.com>
2019-11-26 10:15:47 +0300

    Filter condition in aggregators (partial) #89
    * a slower, but more reusable implementation of filtered aggregation. We will work on its performance separately

--- dflib/src/main/java/com/nhl/dflib/aggregate/AggregatorBuilder.java
+++ dflib/src/main/java/com/nhl/dflib/aggregate/AggregatorBuilder.java
@@ -3,10 +3,6 @@ package com.nhl.dflib.aggregate;
 import com.nhl.dflib.Aggregator;
 import com.nhl.dflib.RowPredicate;
 import com.nhl.dflib.ValuePredicate;
-import com.nhl.dflib.row.RowProxy;
-
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 /**
  * A builder of aggregators that can perform prefiltering and potentially other operations on the DataFrame before
@@ -33,15 +29,13 @@ public class AggregatorBuilder {
         return this;
     }
 
-    protected void appendRowFilter(RowPredicate filter) {
-        this.rowFilter = this.rowFilter != null ? this.rowFilter.and(filter) : filter;
-    }
-
     /**
      * Returns the first value in an aggregation range.
      */
     public <T> Aggregator<T> first(String column) {
         return rowFilter != null
+                // TODO: once the performance TODO in CollectorFilteredAggregator is resolved, perhaps we won't need
+                //  a dedicated FirstFilteredAggregator
                 ? new FirstFilteredAggregator<>(rowFilter, index -> index.position(column), index -> column)
                 : Aggregator.first(column);
     }
@@ -51,6 +45,8 @@ public class AggregatorBuilder {
      */
     public <T> Aggregator<T> first(int column) {
         return rowFilter != null
+                // TODO: once the performance TODO in CollectorFilteredAggregator is resolved, perhaps we won't need
+                //  a dedicated FirstFilteredAggregator
                 ? new FirstFilteredAggregator<>(rowFilter, index -> column, index -> index.getLabel(column))
                 : Aggregator.first(column);
     }
@@ -60,7 +56,7 @@ public class AggregatorBuilder {
      */
     public Aggregator<Long> countLong() {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, countLongCollector(), index -> "_long_count")
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.countLong())
                 : Aggregator.countLong();
     }
 
@@ -69,148 +65,61 @@ public class AggregatorBuilder {
      */
     public Aggregator<Integer> countInt() {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, countIntCollector(), index -> "_int_count")
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.countInt())
                 : Aggregator.countInt();
     }
 
     public Aggregator<Long> sumLong(String column) {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, sumLongCollector(column), index -> column)
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.sumLong(column))
                 : Aggregator.sumLong(column);
     }
 
     public Aggregator<Long> sumLong(int column) {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, sumLongCollector(column), index -> index.getLabel(column))
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.sumLong(column))
                 : Aggregator.sumLong(column);
     }
 
     public Aggregator<Integer> sumInt(String column) {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, sumIntCollector(column), index -> column)
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.sumInt(column))
                 : Aggregator.sumInt(column);
     }
 
     public Aggregator<Integer> sumInt(int column) {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, sumIntCollector(column), index -> index.getLabel(column))
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.sumInt(column))
                 : Aggregator.sumInt(column);
     }
 
     public Aggregator<Long> maxLong(String column) {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, maxLongCollector(column), index -> column)
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.maxLong(column))
                 : Aggregator.maxLong(column);
     }
 
     public Aggregator<Long> maxLong(int column) {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, maxLongCollector(column), index -> index.getLabel(column))
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.maxLong(column))
                 : Aggregator.maxLong(column);
     }
 
 
     public Aggregator<Long> minLong(String column) {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, minLongCollector(column), index -> column)
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.minLong(column))
                 : Aggregator.minLong(column);
     }
 
     public Aggregator<Long> minLong(int column) {
         return rowFilter != null
-                ? new CollectorFilteredAggregator<>(rowFilter, minLongCollector(column), index -> index.getLabel(column))
+                ? new CollectorFilteredAggregator<>(rowFilter, Aggregator.minLong(column))
                 : Aggregator.minLong(column);
     }
 
-    protected Collector<RowProxy, ?, Long> sumLongCollector(int columnPos) {
-        return Collectors.summingLong(r -> ((Number) r.get(columnPos)).longValue());
-    }
 
-    protected Collector<RowProxy, ?, Long> sumLongCollector(String columnLabel) {
-        return Collectors.summingLong(r -> ((Number) r.get(columnLabel)).longValue());
-    }
-
-    protected Collector<RowProxy, ?, Integer> sumIntCollector(int columnPos) {
-        return Collectors.summingInt(r -> ((Number) r.get(columnPos)).intValue());
-    }
-
-    protected Collector<RowProxy, ?, Integer> sumIntCollector(String columnLabel) {
-        return Collectors.summingInt(r -> ((Number) r.get(columnLabel)).intValue());
-    }
-
-    protected Collector<RowProxy, ?, Integer> countIntCollector() {
-        return Collectors.reducing(0, e -> 1, Integer::sum);
-    }
-
-    protected Collector<RowProxy, ?, Long> countLongCollector() {
-        return Collectors.reducing(0L, e -> 1L, Long::sum);
-    }
-
-    protected Collector<RowProxy, ?, Long> maxLongCollector(String columnLabel) {
-        return new AggregationCollector<>(
-                () -> new long[]{Long.MIN_VALUE},
-                (ls, r) -> {
-
-                    Number n = (Number) r.get(columnLabel);
-                    if (n != null) {
-                        long l = n.longValue();
-                        if (ls[0] < l) {
-                            ls[0] = l;
-                        }
-                    }
-                },
-                ls -> ls[0]
-        );
-    }
-
-    protected Collector<RowProxy, ?, Long> maxLongCollector(int columnPos) {
-        return new AggregationCollector<>(
-                () -> new long[]{Long.MIN_VALUE},
-                (ls, r) -> {
-
-                    Number n = (Number) r.get(columnPos);
-                    if (n != null) {
-                        long l = n.longValue();
-                        if (ls[0] < l) {
-                            ls[0] = l;
-                        }
-                    }
-                },
-                ls -> ls[0]
-        );
-    }
-
-    protected Collector<RowProxy, ?, Long> minLongCollector(String columnLabel) {
-        return new AggregationCollector<>(
-                () -> new long[]{Long.MAX_VALUE},
-                (ls, r) -> {
-
-                    Number n = (Number) r.get(columnLabel);
-                    if (n != null) {
-                        long l = n.longValue();
-                        if (ls[0] > l) {
-                            ls[0] = l;
-                        }
-                    }
-                },
-                ls -> ls[0]
-        );
-    }
-
-    protected Collector<RowProxy, ?, Long> minLongCollector(int columnPos) {
-        return new AggregationCollector<>(
-                () -> new long[]{Long.MAX_VALUE},
-                (ls, r) -> {
-
-                    Number n = (Number) r.get(columnPos);
-                    if (n != null) {
-                        long l = n.longValue();
-                        if (ls[0] > l) {
-                            ls[0] = l;
-                        }
-                    }
-                },
-                ls -> ls[0]
-        );
+    protected void appendRowFilter(RowPredicate filter) {
+        this.rowFilter = this.rowFilter != null ? this.rowFilter.and(filter) : filter;
     }
 }
--- dflib/src/main/java/com/nhl/dflib/aggregate/CollectorFilteredAggregator.java
+++ dflib/src/main/java/com/nhl/dflib/aggregate/CollectorFilteredAggregator.java
@@ -4,11 +4,6 @@ import com.nhl.dflib.Aggregator;
 import com.nhl.dflib.DataFrame;
 import com.nhl.dflib.Index;
 import com.nhl.dflib.RowPredicate;
-import com.nhl.dflib.row.RowProxy;
-
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.stream.Collector;
 
 /**
  * @since 0.7
@@ -16,41 +11,30 @@ import java.util.stream.Collector;
 public class CollectorFilteredAggregator<S, A, T> implements Aggregator<T> {
 
     private RowPredicate rowFilter;
-    private Collector<S, A, T> collector;
-    private Function<Index, String> targetColumnNamer;
+    private Aggregator<T> aggregator;
 
-    public CollectorFilteredAggregator(
-            RowPredicate rowFilter,
-            Collector<S, A, T> collector,
-            Function<Index, String> targetColumnNamer) {
-
+    public CollectorFilteredAggregator(RowPredicate rowFilter, Aggregator<T> aggregator) {
         this.rowFilter = rowFilter;
-        this.collector = collector;
-        this.targetColumnNamer = targetColumnNamer;
+        this.aggregator = aggregator;
     }
 
     @Override
     public T aggregate(DataFrame df) {
 
-        BiConsumer accumulator = collector.accumulator();
-        A accumResult = collector.supplier().get();
-
-        for (RowProxy r : df) {
-            if (rowFilter.test(r)) {
-                accumulator.accept(accumResult, r);
-            }
-        }
-
-        return collector.finisher().apply(accumResult);
+        // TODO: we can probably gain significant performance improvements by not creating an intermediate filtered
+        //  DataFrame (as most aggregators will only work with a single column).. At the same time aggregating over
+        //  an Iterator<RowProxy> doesn't allow to reuse Series Aggregators... Need to find a well-performing solution,
+        //  perhaps a lazily-resolved filtered DataFrame
+        return aggregator.aggregate(df.filterRows(rowFilter));
     }
 
     @Override
     public String aggregateLabel(Index columnIndex) {
-        return targetColumnNamer.apply(columnIndex);
+        return aggregator.aggregateLabel(columnIndex);
     }
 
     @Override
     public Aggregator named(String newAggregateLabel) {
-        return new CollectorFilteredAggregator<>(rowFilter, collector, i -> newAggregateLabel);
+        return new CollectorFilteredAggregator<>(rowFilter, aggregator.named(newAggregateLabel));
     }
 }
