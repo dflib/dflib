ddfdac1575dde3623aa650a768e7eee31e27576e
Andrus Adamchik <andrus@objectstyle.com>
2021-01-23 14:20:02 +0300

    comments

--- dflib-avro/src/main/java/com/nhl/dflib/avro/types/BigDecimalConversion.java
+++ dflib-avro/src/main/java/com/nhl/dflib/avro/types/BigDecimalConversion.java
@@ -10,9 +10,8 @@ import java.nio.ByteBuffer;
 /**
  * @since 0.11
  */
-// Avro has its own "decimal" logical type mapping to BigDecimal. It is using "fixed" simple type and mandates
-// scale and precision. We are using ByteBuffer serialization and arbitrary precision. Is there any advantage
-// to "fixed" vs "binary" type?
+// Avro has its own "decimal" logical type mapping to BigDecimal. But it requires predefined scale and precision.
+// This would be too limiting for a DataFrame column
 public class BigDecimalConversion extends SingleSchemaConversion<BigDecimal> {
 
     static final String NAME = "dflib-bigdecimal";
