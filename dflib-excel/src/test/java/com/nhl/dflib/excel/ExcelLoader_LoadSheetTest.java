5f297a92778dc910611e1573713637d204bf0471
Andrus Adamchik <andrus@objectstyle.com>
2021-09-17 11:03:55 +0300

    Support for loading Excel files #136
    .. data types

--- dflib-excel/src/test/java/com/nhl/dflib/excel/ExcelLoader_LoadSheetTest.java
+++ dflib-excel/src/test/java/com/nhl/dflib/excel/ExcelLoader_LoadSheetTest.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -58,8 +59,31 @@ public class ExcelLoader_LoadSheetTest {
     }
 
     @Test
-    public void tesInvalidSheetName() throws URISyntaxException {
+    public void testInvalidSheetName() throws URISyntaxException {
         File file = new File(getClass().getResource("multi-sheet.xlsx").toURI());
         assertThrows(RuntimeException.class, () -> new ExcelLoader().loadSheet(file, "No_such_Sheet"));
+    }
+
+    @Test
+    public void testDataTypes() throws URISyntaxException {
+
+        File file = new File(getClass().getResource("one-sheet-data-types.xlsx").toURI());
+
+        DataFrame df = new ExcelLoader().loadSheet(file, "Sheet1");
+        new DataFrameAsserts(df, "A", "B")
+                .expectHeight(9)
+                .expectRow(0, "Boolean", true)
+                // TODO: can we guess that an int is an int, and not a double?
+                .expectRow(1, "Int", 1d)
+                // TODO: can we guess that a long is a long, and not a double?
+                .expectRow(2, "Long", 2147483647123d)
+                .expectRow(3, "Double", 1.11)
+                .expectRow(4, "Negative Double", -1.111)
+                .expectRow(5, "Currency", 450.99)
+                .expectRow(6, "Percent", 0.88)
+                // TODO: can we tell a Date from Datetime? Parse String manually?
+                .expectRow(7, "Date", LocalDateTime.of(2020, 1, 18, 0, 0, 0))
+                .expectRow(8, "Datetime", LocalDateTime.of(2020, 1, 18, 15, 1, 55));
+
     }
 }
