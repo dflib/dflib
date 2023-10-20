282fb5974091b54cc4772a2354648ca2d081e2a0
Andrus Adamchik <andrus@objectstyle.com>
2023-10-20 15:38:38 -0400

    typo

--- dflib/src/main/java/com/nhl/dflib/row/RowBuilder.java
+++ dflib/src/main/java/com/nhl/dflib/row/RowBuilder.java
@@ -5,7 +5,7 @@ import com.nhl.dflib.Index;
 /**
  * Accumulates user-provided values for a single row. Specific implementations provide their own methods to convert
  * accumulated data into an internal row representation. Just like {@link RowProxy}, {@link RowBuilder} should not be
- * cached or reused outside of the operation where it is exposed, as its state changes between mapper calls.
+ * cached or reused outside the operation where it is exposed, as its state changes between mapper calls.
  */
 public interface RowBuilder {
 
