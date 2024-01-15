4b5e23d390347919580f11b683254855342416dd
Andrus Adamchik <andrus@objectstyle.com>
2024-01-14 19:13:20 -0500

    deprecation note

--- dflib/src/main/java/org/dflib/RowToBooleanValueMapper.java
+++ dflib/src/main/java/org/dflib/RowToBooleanValueMapper.java
@@ -4,7 +4,9 @@ import org.dflib.row.RowProxy;
 
 /**
  * @since 0.6
+ * @deprecated unused anywhere except in deprecated APIs. A complete analog of {@link RowPredicate}
  */
+@Deprecated(since = "1.0.0-M19", forRemoval = true)
 public interface RowToBooleanValueMapper {
 
     boolean map(RowProxy row);
