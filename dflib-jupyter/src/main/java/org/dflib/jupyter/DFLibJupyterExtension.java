fe54cd91e47413be57c9e81a21616f753a203f54
Andrus Adamchik <andrus@objectstyle.com>
2024-09-04 16:47:21 -0400

    Switch to the "org.dflib.jjava:jupyter-jvm-basekernel", turn into jjava extension #321
    adding a static import of Exp

--- dflib-jupyter/src/main/java/org/dflib/jupyter/DFLibJupyterExtension.java
+++ dflib-jupyter/src/main/java/org/dflib/jupyter/DFLibJupyterExtension.java
@@ -8,11 +8,13 @@ import org.dflib.jjava.jupyter.kernel.BaseKernel;
  */
 public class DFLibJupyterExtension implements Extension {
 
-    private static final String STARTUP_SCRIPT =
-                    "import org.dflib.jupyter.*;\n" +
-                    "import org.dflib.*;\n" +
-                    // must call init method here, to process everything in order inside code executor
-                    "DFLibJupyter.init(getKernelInstance());";
+    private static final String STARTUP_SCRIPT = "" +
+            "import org.dflib.jupyter .*;\n" +
+            "import org.dflib.*;\n" +
+            "import static org.dflib.Exp.*;\n" +
+
+            // must call init method here, to process everything in order inside code executor
+            "DFLibJupyter.init(getKernelInstance());";
 
     @Override
     public void install(BaseKernel kernel) {
