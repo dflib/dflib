02c8db5f904ad93843795d65bd991d1d58c02f1c
Andrus Adamchik <andrus@objectstyle.com>
2024-11-02 10:03:42 -0400

    refactoring inheritance hierarchy

--- dflib/src/main/java/org/dflib/exp/agg/DecimalReduceExp1.java
+++ dflib/src/main/java/org/dflib/exp/agg/DecimalReduceExp1.java
@@ -5,7 +5,6 @@ import org.dflib.DataFrame;
 import org.dflib.DecimalExp;
 import org.dflib.Exp;
 import org.dflib.Series;
-import org.dflib.exp.Exp1;
 import org.dflib.series.SingleValueSeries;
 
 import java.math.BigDecimal;
@@ -14,15 +13,10 @@ import java.util.function.Function;
 /**
  * @since 2.0.0
  */
-public class DecimalReduceExp1<F> extends Exp1<F, BigDecimal> implements DecimalExp {
-
-    private final Function<Series<F>, BigDecimal> op;
-    private final Condition filter;
+public class DecimalReduceExp1<F> extends ReduceExp1<F, BigDecimal> implements DecimalExp {
 
     public DecimalReduceExp1(String opName, Exp<F> exp, Function<Series<F>, BigDecimal> op, Condition filter) {
-        super(opName, BigDecimal.class, exp);
-        this.op = op;
-        this.filter = filter;
+        super(opName, BigDecimal.class, exp, op, filter);
     }
 
     @Override
@@ -33,15 +27,5 @@ public class DecimalReduceExp1<F> extends Exp1<F, BigDecimal> implements Decimal
     @Override
     public Series<BigDecimal> eval(DataFrame df) {
         return new SingleValueSeries<>(reduce(df), df.height());
-    }
-
-    @Override
-    public BigDecimal reduce(DataFrame df) {
-        return op.apply(exp.eval(filter != null ? df.rows(filter).select() : df));
-    }
-
-    @Override
-    public BigDecimal reduce(Series<?> s) {
-        return op.apply(exp.eval(filter != null ? s.select(filter) : s));
     }
 }
