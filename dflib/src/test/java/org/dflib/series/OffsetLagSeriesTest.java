b3c5e036098ffff57a6cba0a15ba0e344f78b2b4
Andrus Adamchik <andrus@objectstyle.com>
2024-11-13 18:05:44 -0500

    "Series.shift()" with out of bounds negative shift results in ArrayIndexOutOfBoundsException #390

--- dflib/src/test/java/org/dflib/series/OffsetLagSeriesTest.java
+++ dflib/src/test/java/org/dflib/series/OffsetLagSeriesTest.java
@@ -3,6 +3,8 @@ package org.dflib.series;
 import org.dflib.Series;
 import org.dflib.unit.SeriesAsserts;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,6 +42,25 @@ public class OffsetLagSeriesTest {
         Object[] b2 = new Object[5];
         s.copyTo(b2, 1, 1, 3);
         assertArrayEquals(new Object[]{null, "c", "d", "X", null}, b2);
+
+        Object[] b3 = new Object[5];
+        s.copyTo(b3, 3, 1, 1);
+        assertArrayEquals(new Object[]{null, "X", null, null, null}, b3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-4, -100})
+    public void copyTo_OutOfBoundsOffset(int offset) {
+
+        Series<String> s = new OffsetLagSeries<>(Series.of("a", "b", "c", "d"), offset, "X");
+
+        Object[] b1 = new Object[5];
+        s.copyTo(b1, 0, 1, 4);
+        assertArrayEquals(new Object[]{null, "X", "X", "X", "X"}, b1);
+
+        Object[] b2 = new Object[5];
+        s.copyTo(b2, 1, 1, 3);
+        assertArrayEquals(new Object[]{null, "X", "X", "X", null}, b2);
 
         Object[] b3 = new Object[5];
         s.copyTo(b3, 3, 1, 1);
