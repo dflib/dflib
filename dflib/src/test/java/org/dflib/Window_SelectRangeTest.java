4a2762f17b9e7c45ce7988fd00ccd1969eb3235f
Andrus Adamchik <andrus@objectstyle.com>
2025-01-26 15:28:17 -0500

    refactoring Window range test

--- dflib/src/test/java/org/dflib/Window_SelectRangeTest.java
+++ dflib/src/test/java/org/dflib/Window_SelectRangeTest.java
@@ -6,56 +6,55 @@ import org.junit.jupiter.api.Test;
 
 import static org.dflib.Exp.*;
 
-public class Window_RangeTest {
+public class Window_SelectRangeTest {
+
+    static final DataFrame EMPTY_TEST_DF = DataFrame.empty("a", "b", "c");
+
+    static final DataFrame SINGLE_COL_TEST_DF = DataFrame.foldByRow("val").of(
+            1,
+            22,
+            15,
+            2);
+
+    static final DataFrame TWO_COL_TEST_DF =  DataFrame.foldByRow("order", "val").of(
+            2, 1,
+            4, 22,
+            1, 15,
+            3, 2);
 
     @Test
-    public void all_empty() {
-        DataFrame df = DataFrame.empty("a", "b", "c");
-        DataFrame r = df.over().range(WindowRange.all).select($int("a").sum());
+    public void all_Empty() {
+        DataFrame r = EMPTY_TEST_DF.over().range(WindowRange.all).select($int("a").sum());
         new DataFrameAsserts(r, "sum(a)").expectHeight(0);
     }
 
     @Test
-    public void preceding_empty() {
-        DataFrame df = DataFrame.empty("a", "b", "c");
-        DataFrame r = df.over().range(WindowRange.allPreceding).select($int("a").sum());
+    public void preceding_Empty() {
+        DataFrame r = EMPTY_TEST_DF.over().range(WindowRange.allPreceding).select($int("a").sum());
         new DataFrameAsserts(r, "sum(a)").expectHeight(0);
     }
 
     @Test
-    public void following_empty() {
-        DataFrame df = DataFrame.empty("a", "b", "c");
-        DataFrame r = df.over().range(WindowRange.allFollowing).select($int("a").sum());
+    public void following_Empty() {
+        DataFrame r = EMPTY_TEST_DF.over().range(WindowRange.allFollowing).select($int("a").sum());
         new DataFrameAsserts(r, "sum(a)").expectHeight(0);
     }
 
     @Test
     public void all() {
-        DataFrame df = DataFrame.foldByRow("a", "b").of(
-                1, "x",
-                2, "y",
-                1, "z",
-                0, "a",
-                1, "x");
 
-        DataFrame r = df.over().range(WindowRange.all).select($int("a").sum());
-        new DataFrameAsserts(r, "sum(a)").expectHeight(5)
-                .expectRow(0, 5)
-                .expectRow(1, 5)
-                .expectRow(2, 5)
-                .expectRow(3, 5)
-                .expectRow(4, 5);
+        DataFrame r = TWO_COL_TEST_DF.over().range(WindowRange.all).select($int(0).sum());
+        new DataFrameAsserts(r, "sum(order)")
+                .expectHeight(4)
+                .expectRow(0, 10)
+                .expectRow(1, 10)
+                .expectRow(2, 10)
+                .expectRow(3, 10);
     }
 
     @Test
-    public void range_Preceding() {
-        DataFrame df = DataFrame.foldByRow("val").of(
-                1,
-                22,
-                15,
-                2);
-
-        DataFrame r = df.over().range(WindowRange.allPreceding).select($int("val").sum());
+    public void preceding() {
+        DataFrame r = SINGLE_COL_TEST_DF.over().range(WindowRange.allPreceding).select($int("val").sum());
         new DataFrameAsserts(r, "sum(val)").expectHeight(4)
                 .expectRow(0, 1)
                 .expectRow(1, 23)
@@ -64,14 +63,8 @@ public class Window_RangeTest {
     }
 
     @Test
-    public void range_Preceding_MultiAgg() {
-        DataFrame df = DataFrame.foldByRow("val").of(
-                1,
-                22,
-                15,
-                2);
-
-        DataFrame r = df.over().range(WindowRange.allPreceding).select(
+    public void preceding_MultiExp() {
+        DataFrame r = SINGLE_COL_TEST_DF.over().range(WindowRange.allPreceding).select(
                 $int("val").sum(),
                 $int("val").max());
 
@@ -83,14 +76,8 @@ public class Window_RangeTest {
     }
 
     @Test
-    public void range_Following() {
-        DataFrame df = DataFrame.foldByRow("val").of(
-                1,
-                22,
-                15,
-                2);
-
-        DataFrame r = df.over().range(WindowRange.allFollowing).select($int("val").sum());
+    public void following() {
+        DataFrame r = SINGLE_COL_TEST_DF.over().range(WindowRange.allFollowing).select($int("val").sum());
         new DataFrameAsserts(r, "sum(val)").expectHeight(4)
                 .expectRow(0, 40)
                 .expectRow(1, 39)
@@ -99,14 +86,9 @@ public class Window_RangeTest {
     }
 
     @Test
-    public void sorted_Range_Preceding() {
-        DataFrame df = DataFrame.foldByRow("order", "val").of(
-                2, 1,
-                4, 22,
-                1, 15,
-                3, 2);
+    public void preceding_Sorted() {
 
-        DataFrame r = df.over()
+        DataFrame r = TWO_COL_TEST_DF.over()
                 .sorted($col("order").asc())
                 .range(WindowRange.allPreceding)
                 .select($int("val").sum());
@@ -119,14 +101,9 @@ public class Window_RangeTest {
     }
 
     @Test
-    public void sorted_Range_Following() {
-        DataFrame df = DataFrame.foldByRow("order", "val").of(
-                2, 1,
-                4, 22,
-                1, 15,
-                3, 2);
+    public void following_Sorted() {
 
-        DataFrame r = df.over()
+        DataFrame r = TWO_COL_TEST_DF.over()
                 .sorted($col("order").asc())
                 .range(WindowRange.allFollowing)
                 .select($int("val").sum());
@@ -138,9 +115,8 @@ public class Window_RangeTest {
                 .expectRow(3, 22);
     }
 
-
     @Test
-    public void partitioned_Sorted_Range_Preceding() {
+    public void preceding_Partitioned_Sorted() {
         DataFrame df = DataFrame.foldByRow("label", "order", "val").of(
                 "a", 2, 1,
                 "a", 3, 22,
