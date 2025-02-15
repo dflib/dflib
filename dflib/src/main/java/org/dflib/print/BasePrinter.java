125eb0f183358a8a9911ad4d9ad166526d91eb47
Andrus Adamchik <andrus@objectstyle.com>
2025-02-15 17:21:45 -0500

    Align display defaults between core Printers and Jupyter #465

--- dflib/src/main/java/org/dflib/print/BasePrinter.java
+++ dflib/src/main/java/org/dflib/print/BasePrinter.java
@@ -2,7 +2,7 @@ package org.dflib.print;
 
 public abstract class BasePrinter implements Printer {
 
-    private static final int MAX_DISPLAY_ROWS = 5;
+    private static final int MAX_DISPLAY_ROWS = 10;
     private static final int MAX_DISPLAY_COLUMN_WIDTH = 50;
 
     protected int maxDisplayRows;
