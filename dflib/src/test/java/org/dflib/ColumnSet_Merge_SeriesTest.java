a88b2e290c422d5826320ef215f0b3d2fbff5273
Andrus Adamchik <andrus@objectstyle.com>
2025-03-02 14:45:10 -0500

    adding missing tests

---
+++ dflib/src/test/java/org/dflib/ColumnSet_Merge_SeriesTest.java
@@ -0,0 +1,48 @@
+package org.dflib;
+
+import org.dflib.unit.DataFrameAsserts;
+import org.junit.jupiter.api.Test;
+
+public class ColumnSet_Merge_SeriesTest {
+
+    @Test
+    public void all() {
+        DataFrame df = DataFrame.foldByRow("a", "b")
+                .of(1, "x", 2, "y")
+                .cols()
+                .merge(Series.of("A", "B"));
+
+        new DataFrameAsserts(df, "a", "b", "2")
+                .expectHeight(2)
+                .expectRow(0, 1, "x", "A")
+                .expectRow(1, 2, "y", "B");
+    }
+
+    @Test
+    public void byName() {
+        DataFrame df = DataFrame.foldByRow("a", "b")
+                .of(1, "x", 2, "y")
+                .cols("b")
+                .merge(Series.of("A", "B"));
+
+        new DataFrameAsserts(df, "a", "b")
+                .expectHeight(2)
+                .expectRow(0, 1, "A")
+                .expectRow(1, 2, "B");
+    }
+
+    @Test
+    public void byName_Duplicate() {
+        DataFrame df = DataFrame.foldByRow("a", "b")
+                .of(1, "x", 2, "y")
+                .cols("b", "b").merge(
+                        Series.of("A", "B"),
+                        Series.of("C", "D")
+                );
+
+        new DataFrameAsserts(df, "a", "b", "b_")
+                .expectHeight(2)
+                .expectRow(0, 1, "A", "C")
+                .expectRow(1, 2, "B", "D");
+    }
+}
