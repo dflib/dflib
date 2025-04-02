936341950417da0b43c2b2c0eefdca46e7859440
Andrus Adamchik <andrus@objectstyle.com>
2025-04-02 17:36:14 -0400

    vConcat loses the type of the column #477
    aligning VConcat with the latest optimizations

--- dflib/src/main/java/org/dflib/concat/SeriesConcat.java
+++ dflib/src/main/java/org/dflib/concat/SeriesConcat.java
@@ -49,25 +49,6 @@ public class SeriesConcat {
     }
 
     public static IntSeries intConcat(Iterable<IntSeries> concat) {
-        int h = 0;
-        int total = 0;
-        for (IntSeries s : concat) {
-            h += s.size();
-            total++;
-        }
-
-        if (total == 1) {
-            return concat.iterator().next();
-        }
-
-        int[] data = new int[h];
-        int offset = 0;
-        for (IntSeries s : concat) {
-            int len = s.size();
-            s.copyToInt(data, 0, offset, len);
-            offset += len;
-        }
-
-        return Series.ofInt(data);
+        return intConcat(StreamSupport.stream(concat.spliterator(), false).toArray(IntSeries[]::new));
     }
 }
