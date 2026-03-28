41a5048a24cb86ea18686cca08f2ebc3f4ef03d8
Andrus Adamchik <andrus@objectstyle.com>
2026-03-28 09:41:59 -0400

    cleanup

--- dflib-jdbc/src/test/java/org/dflib/jdbc/unit/JdbcContainerTool.java
+++ dflib-jdbc/src/test/java/org/dflib/jdbc/unit/JdbcContainerTool.java
@@ -10,24 +10,23 @@ public class JdbcContainerTool {
 
     private static final String MYSQL_PASSWORD = "test";
 
-    private String dbType;
-    private JdbcDatabaseContainer maybeContainer;
+    private final String dbType;
+    private final JdbcDatabaseContainer maybeContainer;
 
     public static JdbcContainerTool create(String dbType) {
-        switch (dbType) {
 
-            // MySQL is special ... We need a "root" user to setup schema, and that requires an explicit container object
-            case "mysql":
-                JdbcDatabaseContainer container = new MySQLContainer("mysql:8.0.20").withPassword(MYSQL_PASSWORD);
+        // MySQL is special ... We need a "root" user to set up schema, and that requires an explicit container object
+        if (dbType.equals("mysql")) {
+            JdbcDatabaseContainer container = new MySQLContainer("mysql:8.0.20").withPassword(MYSQL_PASSWORD);
 
-                // we are working in a GLOBAL scope, so just start the DB as early as we can, and don't worry about
-                // shutdown...
-                container.start();
+            // we are working in a GLOBAL scope, so just start the DB as early as we can, and don't worry about
+            // shutdown...
+            container.start();
 
-                return new JdbcContainerTool(dbType, container);
-            default:
-                return new JdbcContainerTool(dbType, null);
+            return new JdbcContainerTool(dbType, container);
         }
+
+        return new JdbcContainerTool(dbType, null);
     }
 
     protected JdbcContainerTool(String dbType, JdbcDatabaseContainer maybeContainer) {
@@ -36,22 +35,18 @@ public class JdbcContainerTool {
     }
 
     public DbTester createDbTester() {
-        switch (dbType) {
-            case "mysql":
-                return TcDbTester
-                        // password is the same for "root", as the one for non-root user above
-                        .db(maybeContainer, "root", MYSQL_PASSWORD)
-                        .initDB("classpath:org/dflib/jdbc/init_schema_mysql.sql", "--");
-            case "postgresql":
-                return TcDbTester
-                        .db("jdbc:tc:postgresql:11://localhost/test")
-                        .initDB("classpath:org/dflib/jdbc/init_schema_postgresql.sql", "--");
-            case "derby":
-                return DerbyTester
-                        .db()
-                        .initDB("classpath:org/dflib/jdbc/init_schema_derby.sql", "--");
-            default:
-                throw new IllegalArgumentException("Unsupported DB type: " + dbType);
-        }
+        return switch (dbType) {
+            case "mysql" -> TcDbTester
+                    // password is the same for "root", as the one for non-root user above
+                    .db(maybeContainer, "root", MYSQL_PASSWORD)
+                    .initDB("classpath:org/dflib/jdbc/init_schema_mysql.sql", "--");
+            case "postgresql" -> TcDbTester
+                    .db("jdbc:tc:postgresql:11://localhost/test")
+                    .initDB("classpath:org/dflib/jdbc/init_schema_postgresql.sql", "--");
+            case "derby" -> DerbyTester
+                    .db()
+                    .initDB("classpath:org/dflib/jdbc/init_schema_derby.sql", "--");
+            default -> throw new IllegalArgumentException("Unsupported DB type: " + dbType);
+        };
     }
 }
