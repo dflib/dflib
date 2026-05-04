71b2ea9a2f5f6222f97e0f631f8ce8fe97115faa
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2026-05-04 23:03:01 +0300

    test: Parametrize CsvSaver tests

--- dflib-csv/src/test/java/org/dflib/csv/CsvSaver_FormatTest.java
+++ dflib-csv/src/test/java/org/dflib/csv/CsvSaver_FormatTest.java
@@ -5,9 +5,13 @@ import org.dflib.csv.parser.format.CsvFormat;
 import org.dflib.csv.parser.format.Escape;
 import org.dflib.csv.parser.format.LineBreak;
 import org.dflib.csv.parser.format.Quote;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
 
 public class CsvSaver_FormatTest {
 
@@ -15,78 +19,54 @@ public class CsvSaver_FormatTest {
     private static final DataFrame DF = DataFrame.foldByRow("A", "B").of(
             1, 2,
             3, 4);
-    
 
-    @Test
-    public void defaultFormat() {
-        assertEquals("A,B\n1,2\n3,4\n", Csv.saver().format(DEFAULT_FORMAT).saveToString(DF));
+    @ParameterizedTest(name = "{0}")
+    @MethodSource
+    public void saveToString(String name, CsvFormat format, DataFrame df, String expected) {
+        assertEquals(expected, Csv.saver().format(format).saveToString(df));
     }
 
-    @Test
-    public void minimalQuoting_specialChars() {
-        DataFrame df = DataFrame.foldByRow("A", "B").of(
-                "x,y", "z\"w",
-                "a\nb", "c\rd");
-
-        assertEquals("A,B\n\"x,y\",\"z\"\"w\"\n\"a\nb\",\"c\rd\"\n",
-                Csv.saver().format(DEFAULT_FORMAT).saveToString(df));
-    }
-
-    @Test
-    public void quoteAlways() {
-        CsvFormat fmt = CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).quote(Quote.of('"')).build();
-
-        assertEquals("\"A\",\"B\"\n\"1\",\"2\"\n\"3\",\"4\"\n",
-                Csv.saver().format(fmt).saveToString(DF));
-    }
-
-    @Test
-    public void escapeBackslash_inQuotedField() {
-        DataFrame df = DataFrame.foldByRow("A").of("z\"w");
-        CsvFormat fmt = CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).escape(Escape.BACKSLASH).build();
-
-        assertEquals("A\n\"z\\\"w\"\n", Csv.saver().format(fmt).saveToString(df));
-    }
-
-    @Test
-    public void escapeCustom_inQuotedField() {
-        DataFrame df = DataFrame.foldByRow("A").of("z\"w");
-        CsvFormat fmt = CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).escape('$').build();
-
-        assertEquals("A\n\"z$\"w\"\n", Csv.saver().format(fmt).saveToString(df));
-    }
-
-    @Test
-    public void nullString() {
-        DataFrame df = DataFrame.foldByRow("A", "B").of(
-                "x", null,
-                null, "y");
-        CsvFormat fmt = CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).nullString("NULL").build();
-
-        assertEquals("A,B\nx,NULL\nNULL,y\n", Csv.saver().format(fmt).saveToString(df));
-    }
-
-    @Test
-    public void trailingDelimiter() {
-        CsvFormat fmt = CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).trailingDelimiter(true).build();
-
-        assertEquals("A,B,\n1,2,\n3,4,\n", Csv.saver().format(fmt).saveToString(DF));
-    }
-
-    @Test
-    public void customDelimiter() {
-        CsvFormat fmt = CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).delimiter(";").build();
-
-        assertEquals("A;B\n1;2\n3;4\n", Csv.saver().format(fmt).saveToString(DF));
-    }
-
-    @Test
-    public void quoteNone_plainData() {
-        CsvFormat fmt = CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT)
-                .quote(Quote.none())
-                .escape(Escape.NONE)
-                .build();
-
-        assertEquals("A,B\n1,2\n3,4\n", Csv.saver().format(fmt).saveToString(DF));
+    private static Stream<Arguments> saveToString() {
+        return Stream.of(
+                Arguments.of("default format",
+                        DEFAULT_FORMAT,
+                        DF,
+                        "A,B\n1,2\n3,4\n"),
+                Arguments.of("minimal quoting for special chars",
+                        DEFAULT_FORMAT,
+                        DataFrame.foldByRow("A", "B").of("x,y", "z\"w", "a\nb", "c\rd"),
+                        "A,B\n\"x,y\",\"z\"\"w\"\n\"a\nb\",\"c\rd\"\n"),
+                Arguments.of("quote always",
+                        CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).quote(Quote.of('"')).build(),
+                        DF,
+                        "\"A\",\"B\"\n\"1\",\"2\"\n\"3\",\"4\"\n"),
+                Arguments.of("escape backslash in quoted field",
+                        CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).escape(Escape.BACKSLASH).build(),
+                        DataFrame.foldByRow("A").of("z\"w"),
+                        "A\n\"z\\\"w\"\n"),
+                Arguments.of("escape custom in quoted field",
+                        CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).escape('$').build(),
+                        DataFrame.foldByRow("A").of("z\"w"),
+                        "A\n\"z$\"w\"\n"),
+                Arguments.of("null string",
+                        CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).nullString("NULL").build(),
+                        DataFrame.foldByRow("A", "B").of("x", null, null, "y"),
+                        "A,B\nx,NULL\nNULL,y\n"),
+                Arguments.of("trailing delimiter",
+                        CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).trailingDelimiter(true).build(),
+                        DF,
+                        "A,B,\n1,2,\n3,4,\n"),
+                Arguments.of("custom delimiter",
+                        CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT).delimiter(";").build(),
+                        DF,
+                        "A;B\n1;2\n3;4\n"),
+                Arguments.of("quote none for plain data",
+                        CsvFormat.defaultFormat().copyFrom(DEFAULT_FORMAT)
+                                .quote(Quote.none())
+                                .escape(Escape.NONE)
+                                .build(),
+                        DF,
+                        "A,B\n1,2\n3,4\n")
+        );
     }
 }
